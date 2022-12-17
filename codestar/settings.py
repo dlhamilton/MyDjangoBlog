2dc6616478feacf563d912fd1d8a55061d83e930
Dan Hamilton <hamiltondan93@gmail.com>
2022-12-17 00:23:23 +0000

    Add: google login

--- codestar/settings.py
+++ codestar/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -57,12 +58,28 @@ INSTALLED_APPS = [
     'blog',
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'offline',
+        },
+        'APP': {
+            "client_id": os.environ["GOOGLE_OAUTH_CLIENT_ID"],
+            "secret": os.environ["GOOGLE_OAUTH_SECRET"]
+            }
+    }
+}
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
